f09a50f73b2abd5dfe8c0834c097d4373ab21f78
ShahzodZiyodullayev <sshahzod5@gmail.com>
2022-05-15 03:09:50 +0500

    create database

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const JSONData = require("./data.json")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const JSONData = require("./data.json");
 
-app.use(cors())
+app.use(cors());
+dotenv.config({ path: "./config.env" });
+
+mongoose.connect(process.env.DATABASE_LOCAL, () =>
+  console.log("DB connection succesfully"),
+);
 
 app.get("/ok", (req, res) => {
-  res.send(JSONData)
-})
+  res.send(JSONData);
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(2000, () => {
-  console.log("Server is running on 2000 port...")
-})
+  console.log(`Server is running on ${port} port...`);
+});
