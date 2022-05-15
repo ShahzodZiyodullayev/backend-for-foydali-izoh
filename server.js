93d14ff2d9d064e240bfdd28e0eb0b6f5d6df79f
ShahzodZiyodullayev <sshahzod5@gmail.com>
2022-05-16 03:35:12 +0500

    frontend bilan axios orqali  aloqa o'rnatildi

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const JSONData = require("./data.json");
+const bodyParser = require("body-parser");
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 dotenv.config({ path: "./config.env" });
 
 mongoose.connect(process.env.DATABASE_LOCAL, () =>
@@ -18,8 +21,13 @@ app.get("/ok", (req, res) => {
   res.send(JSONData);
 });
 
+app.post("/hi", (req, res) => {
+  console.log(req.body);
+  res.send("hii");
+});
+
 const port = process.env.PORT || 5000;
 
-app.listen(2000, () => {
+app.listen(port, () => {
   console.log(`Server is running on ${port} port...`);
 });
