a8b79b76bce2d73b7de2f097a2362cad26cfe836
ShahzodZiyodullayev <sshahzod5@gmail.com>
2022-05-16 14:19:10 +0500

    login qo'shildi

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const JSONData = require("./data.json");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 dotenv.config({ path: "./config.env" });
 
 mongoose.connect(process.env.DATABASE_LOCAL, () =>
   console.log("DB connection succesfully"),
 );
-
-// app.use("/", require)
 
 app.get("/ok", (req, res) => {
   res.send(JSONData);
@@ -23,7 +23,20 @@ app.get("/ok", (req, res) => {
 
 app.post("/hi", (req, res) => {
   console.log(req.body);
-  res.send("hii");
+  const { password, login } = req.body;
+  if (req.body && login === "shahzod" && password === "123") {
+    res.status(200).json({
+      succes: true,
+      data: {
+        id: 123456789,
+        name: "Shahzod",
+      },
+    });
+  } else {
+    res.status(404).json({
+      succes: false,
+    });
+  }
 });
 
 const port = process.env.PORT || 5000;
